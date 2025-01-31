c6e37f99c0b43733dafb5482a9a9d193bd636926
solomon613 <149439040+solomon613@users.noreply.github.com>
2025-01-31 13:12:12 -0800

    Delete Quit Habit Website directory

--- Quit Habit Website/scripts/mobile-menu.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const mobileMenuButton = document.querySelector('.mobile-menu-button');
-    const navLinks = document.querySelector('.nav-links');
-
-    mobileMenuButton.addEventListener('click', () => {
-        navLinks.classList.toggle('show');
-        mobileMenuButton.classList.toggle('active');
-    });
-}); 
